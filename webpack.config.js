212ff5c0fefe8287200d57d41afab602e0442635
JYC <mobilechoi@ajou.ac.kr>
2023-04-11 15:43:59 +0900

    match versions

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,28 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
-  // enntry file
-  entry: './src/index.js',
-  // 컴파일 + 번들링된 js 파일이 저장될 경로와 이름 지정
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        include: [
-          path.resolve(__dirname, 'src')
-        ],
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env', '@babel/preset-react'],
-            plugins: ['@babel/plugin-proposal-class-properties']
-          }
-        }
-      }
+    entry: {
+        app: './src/index.js'
+    },
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'public')
+    },
+    module: {
+        rules: [{
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader'
+            }
+        }]
+    },
+    devServer: {
+        hot: true,
+        allowedHosts: 'all',
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
     ]
-  },
-  devtool: 'source-map',
-  // https://webpack.js.org/concepts/mode/#mode-development
-  mode: 'development'
 };
-
