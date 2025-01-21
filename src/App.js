eebee75f0385fa403ee51092405b6e465722f516
sebeeeen <a856412@gmail.com>
2025-01-21 14:54:13 +0900

    [Feat] Add Home page

--- src/App.js
+++ src/App.js
@@ -1,35 +1,35 @@
+// src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { CssBaseline, Box } from '@mui/material';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import PortfolioList from './components/portfolio/PortfolioList';
-
-function Home() {
-  return (
-    <div style={{ paddingTop: '80px' }}>
-      <h1>홈 화면</h1>
-      {/* 컨텐츠 추가 */}
-    </div>
-  );
-}
+import Home from './pages/Home';
 
 export default function App() {
   return (
     <Router>
-      <div style={{ 
+      <CssBaseline />
+      <Box sx={{ 
         display: 'flex',
         flexDirection: 'column',
         minHeight: '100vh'
       }}>
         <Navbar />
-        <main style={{ flex: 1 }}>
+        <Box component="main" sx={{ 
+          flex: 1,
+          display: 'flex',
+          flexDirection: 'column',
+          marginTop: '64px' // Navbar 높이만큼 여백
+        }}>
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/portfolio" element={<PortfolioList />} />
           </Routes>
-        </main>
+        </Box>
         <Footer />
-      </div>
+      </Box>
     </Router>
   );
 }
