72096473a780e5895ea8ba9dc7865904df70c602
1-J-1 <nowijnah15@gmail.com>
2025-02-08 01:04:04 +0900

    signup&signin

---
+++ src/firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyABxF3nO6fVtgk-RQEwE5eYK5GKEt8diEI",
+  authDomain: "ajou-project-cafd9.firebaseapp.com",
+  projectId: "ajou-project-cafd9",
+  storageBucket: "ajou-project-cafd9.firebasestorage.app",
+  messagingSenderId: "926871086070",
+  appId: "1:926871086070:web:9a01bad8170cb172f15dd7",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+
+export default app;
